cd65898cb907a549005e4214359fda1c12ebf179
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-09 09:34:37 +0900

    workaround

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,1 +1,10 @@
 import 'jest-preset-angular/setup-jest';
+
+// Pretending to be jasmine
+
+// if (!Object.prototype.hasOwnProperty.call(window, 'jasmine')) {
+//   console.log('Pretending to be jasmine');
+//   Object.defineProperty(window, 'jasmine', {
+//     value: true,
+//   });
+// }
